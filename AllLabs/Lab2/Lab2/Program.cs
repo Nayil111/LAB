f5923588bb9b9426ea5d7b3b8487fd00854b8c4b
Nayil <n_kinzhinov@mail.ru>
2022-12-07 18:06:14 +0300

    AllLabsUpdate

---
+++ AllLabs/Lab2/Lab2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число от 1 до 7");
+            int num = 0;
+            while (true)
+            {
+                string str = Console.ReadLine();
+                bool res = int.TryParse(str, out num);
+                if ((res == true))
+                {
+                    if ((num < 8) && (num > -1))
+                    {
+                        string month = "";
+
+                        switch (num)
+
+                        {
+                            case 1:
+                                month = "понедельник";
+                                break;
+                            case 2:
+                        }
+    }
+}
